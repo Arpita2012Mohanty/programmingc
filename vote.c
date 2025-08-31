1ae04d46a184bc40aae5cf9668d5b8d305836224
Arpita2012Mohanty <250320100046@cutm.ac.in>
2025-08-31 23:09:51 +0530

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("enter your age\n");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("eligible to vote\n");
+
+    }
+    else
+    {
+        printf("not eligible to vote\n");
+    }
+    return 0;
+}
