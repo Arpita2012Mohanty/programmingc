03740313f0281170eea044911f33f5a5d2fc3306
Arpita2012Mohanty <250320100046@cutm.ac.in>
2025-09-01 08:37:57 +0530

    Add files via upload

---
+++ palindrom.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,rev=0,rem,original;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    original=n;
+    while(n!=0)
+    {
+        rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    if(original==rev)
+        printf("The number is a palindrome.\n");
+    else
+        printf("The number is not a palindrome.\n");
+    return 0;
+}
